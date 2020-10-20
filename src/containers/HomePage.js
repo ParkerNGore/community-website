72d64e131f309055c458dea0a4f644d22763806f
CloakingOcean <shawn.w8465@gmail.com>
2020-10-19 21:21:57 -0600

    Seems to be working for the most part
    -Still need to implement redirection

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -7,6 +7,13 @@ function HomePage() {
     <div className="home-page-container">
       <main>
         <h1>Welcome to the Home-Page!</h1>
+        <a
+          href={`http://localhost:${
+            process.env.PORT ? process.env.PORT : 3005
+          }/api/oauth2/login`}
+        >
+          <h1>Click Me</h1>
+        </a>
       </main>
     </div>
   );
