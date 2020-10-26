5064cd901556fa35976028e096cbbb42987ece5a
CloakingOcean <shawn.w8465@gmail.com>
2020-10-26 05:21:09 -0600

    Closes #6 - HomePage (MVP)
    Adds a brief introduction to the HomePage

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -7,13 +7,18 @@ function HomePage() {
     <div className="home-page-container">
       <main>
         <h1>Welcome to the Home-Page!</h1>
-        <a
-          href={`http://localhost:${
-            process.env.PORT ? process.env.PORT : 3005
-          }/api/oauth2/login`}
-        >
-          <h1>Click Me</h1>
-        </a>
+        <main>
+          <section>
+            <p>
+              You've stumbled upon our exciting new project, Welcome! We hope to
+              have you join us watching this app come together over time! Please
+              let us know any feedback you have at{" "}
+              <a href="https://github.com/ParkerNGore/community-website/issues">
+                Our Front-End Repository!
+              </a>
+            </p>
+          </section>
+        </main>
       </main>
     </div>
   );
